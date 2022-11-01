0d5c143c12e8f33db273261efbdee2b5b7f6736f
Russell Smith <resmithjr88@gmail.com>
2022-11-01 04:11:02 -0600

    Desired result appears in console log when run with node. Jest fails to determine test suites to run on npm test

--- src/combinedLists.js
+++ src/combinedLists.js
@@ -1,3 +1,5 @@
+const { januaryTotals, februaryTotals } = require("../src/data")
+
 function combineMonthlyRainfallTotals(firstMonthTotals, secondMonthTotals) {
   // Write your solution to combine the Lists of Monthly Rainfall Totals here.
   // The correct result will not have duplicates and will appropriately add rainfall amounts from cities that appear in both lists.
@@ -9,19 +11,22 @@ function combineMonthlyRainfallTotals(firstMonthTotals, secondMonthTotals) {
   let endResult = firstMonthTotals.asArray()
   let secondMonth = secondMonthTotals.asArray()
   let compare = endResult.map(cityDetails => cityDetails.city)
-  console.log(compare)
 
   secondMonth.forEach(cityInQuestion => {
-    const cityName = cityInQuestion.name
+    const cityName = cityInQuestion.city
     if (compare.includes(cityName)) {
-      endResult[cityName].rainfall += cityInQuestion.rainfall
+      chosenCity = endResult.find(cityObject => cityObject.city === cityName)
+      chosenCity.rainfall += cityInQuestion.rainfall
+      console.log("Chosen City", chosenCity)
     } else {
       endResult.push(cityInQuestion)
+      console.log("Additional city", endResult)
     }
     
   })
-  console.log("endResult", endResult)
   return endResult
 }
+//console.log("January", januaryTotals.asArray(), "February", februaryTotals.asArray())
+console.log(combineMonthlyRainfallTotals(januaryTotals, februaryTotals))
 
 module.exports = combineMonthlyRainfallTotals;
